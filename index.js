1ffb27bafa883c96273cfaf190295c6e3e74eb66
sin wan wildy <sw.wildy@gmail.com>
2021-02-22 21:18:32 -0800

    commit tree 9ddf5932d1d9fc2f78355386754f3935fb893da8

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+let main = document.querySelector('main#main').remove(); 
+
+let newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+document.body.appendChild(newHeader);
+newHeader.innerHTML = "Sin is the champion";
